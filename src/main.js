b3669e2ddc4af629cd9160ad6f0f752d4e17247e
Em Orendorff <em.orendorff@gmail.com>
2021-03-31 19:16:56 -0500

    Access random cover button and add event listener

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ var coverImg = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
-
+var randomCoverButton = document.querySelector('.random-cover-button');
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var titleIndex = getRandomIndex(titles);
 var coverIndex = getRandomIndex(covers);
+var titleIndex = getRandomIndex(titles);
 var taglineIndex1 = getRandomIndex(descriptors);
 var taglineIndex2 = getRandomIndex(descriptors);
 
@@ -22,10 +22,12 @@ descriptor1.innerText = currentCover.tagline1;
 descriptor2.innerText = currentCover.tagline2;
 
 // Add your event listeners here 👇
-
+randomCoverButton.addEventListener('click', generateCover)
 
 // Create your event handlers and other functions here 👇
+function generateCover() {
 
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
