a6c6314f4aa3f9d98419c64be67b9a080ad632bd
Taylor Galloway <gallowaytaylor@outlook.com>
2021-04-04 18:14:32 -0500

    Fix HTML indentation

--- src/main.js
+++ src/main.js
@@ -120,7 +120,12 @@ function displaySavedCovers() {
         <section class="mini-cover" id = "${savedCovers[i].id}">
           <img class="cover-image" src="${savedCovers[i].cover}">
           <h2 class="cover-title">${savedCovers[i].title}</h2>
-          <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+          <h3 class="tagline">
+            A tale of
+            <span class="tagline-1">${savedCovers[i].tagline1}</span>
+            and
+            <span class="tagline-2">${savedCovers[i].tagline2}</span>
+          </h3>
           <img class="price-tag" src="./assets/price.png">
           <img class="overlay" src="./assets/overlay.png">
         </section>
